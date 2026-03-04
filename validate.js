157e943c32896574c0ff81e443faea7c4c256106
Tanadol <oakjatawattana@gmail.com>
2026-03-04 13:49:14 +0700

    Bug fix#2 - adding student id validation (id must be composed of numbers between 0 and 9)

--- validate.js
+++ validate.js
@@ -1,32 +1,32 @@
 function checkSID() {
-  let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
-    return true;
-  } else {
-    return false;
-  }
+	let sid = (document.getElementById("sid").value).trim();
+	if (sid.length == 10 && /^[0-9]+$/.test(sid)) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 function checkCandiNo() {
-  let candi = (document.getElementById("candi").value).trim();
-  if (isNaN(candi) || candi < 1) {
-    return false;
-  } else {
-    return true;
-  }
+	let candi = (document.getElementById("candi").value).trim();
+	if (isNaN(candi) || candi < 1) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
-function validateForm(){
-	if(!checkSID()){
-	  alert("Invalid value for Student ID!");
-	  document.getElementById("sid").focus();
-	  return false;
-	}else{
-		if(!checkCandiNo()){
-		  alert("Invalid value for Candidate No!!");
-		  document.getElementById("candi").focus();
-		  return false;
-		}else{
+function validateForm() {
+	if (!checkSID()) {
+		alert("Invalid value for Student ID!");
+		document.getElementById("sid").focus();
+		return false;
+	} else {
+		if (!checkCandiNo()) {
+			alert("Invalid value for Candidate No!!");
+			document.getElementById("candi").focus();
+			return false;
+		} else {
 			alert("Your input data passes validation!!");
 			return true;
 		}
